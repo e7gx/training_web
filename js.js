268d724e600da421c68267199216a74c1ef4366b
e7gx <abalgabo@gmail.com>
2023-12-25 15:50:39 +0300

    /

--- js.js
+++ js.js
@@ -1,14 +1,25 @@
-let color='hrllo';
+/*let abdullahAge = 23
+const yaerOfBirth = 2001
 
-console.log(color);
-console.log(color);
-console.log(color);
-console.log(color);
-console.log(color);
-console.log(color);
-console.log(color);
-console.log(color);
-console.log(color);
 
-color = 'hi';
-console.log(color);
+console.log(yaerOfBirth);
+console.log(abdullahAge);
+
+let result = 6 == 3;
+console.log(result);
+
+
+let notRaining = true;
+let notFoggy = true;
+let isSunny = notRaining && notFoggy;
+console.log(isSunny);
+
+
+let number = 2;
+let result1 = ++number + 4; // prefix
+console.log(result1);
+console.log(number);
+
+*/
+let value;
+console.log(typeof value);
