5ec7b5357f1f91f62f4f47c6828ff9badaf00d31
e7gx <abalgabo@gmail.com>
2023-12-26 00:33:24 +0300

    ...

--- js.js
+++ js.js
@@ -20,6 +20,36 @@ let result1 = ++number + 4; // prefix
 console.log(result1);
 console.log(number);
 
-*/
+
+console.log('A\tB\tC\tD\tE');
+console.log('A\nB\nC\nD\nE');
+console.log('A\tB\tC\tD\tE');
+console.log('A\nB\nC\nD\nE');
+console.log('A\tB\tC\tD\tE');
+let language = 'JavaScript';
+console.log(language[2]);
+
 let value = null;
 console.log( value);
+
+let a = 0;
+let b = false;
+console.log(a == b)
+console.log(100 % 5)
+
+let name = 'Nasser';
+console.log(name.length);
+
+let password = '_@2#b?';
+if(password.length >= 8){
+   console.log("The password is accepted") ;
+}else { console.log("badd")}*/
+
+let colors = ["red", "green", "blue"];
+
+colors[1] = 'black';
+console.log(colors[0]);
+
+console.log(colors.length);
+console.log(colors.includes('green'));
+console.log(colors.includes('black'));
