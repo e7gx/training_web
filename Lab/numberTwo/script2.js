f5591348d052f769ff26548d8384a91f57bd980f
e7gx <abalgabo@gmail.com>
2024-01-26 06:19:29 +0300

    done

--- Lab/numberTwo/script2.js
+++ Lab/numberTwo/script2.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const countrySelect = document.getElementById('select');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting until we've done our validation
+        event.preventDefault(); //! Prevent the form from submitting until we've done our validation
 
         const phone = phoneInput.value;
-        const country = countrySelect.value; // Use value to get the option value
+        const country = countrySelect.value; //! Use value to get the option value
 
         let phoneRegex;
         switch (country) {
@@ -21,21 +21,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 phoneRegex = /^(\+44)[\s-]?\d{8,12}$/;
                 break;
             default:
-                // Handle the case where no country is selected or a country without a specific regex
+                //! Handle the case where no country is selected or a country without a specific regex
                 alert('Please select a country and enter a valid phone number.');
                 return false;
         }
 
-        // Now check the validity only if phoneRegex has been defined
         if (phoneRegex && !phoneRegex.test(phone)) {
             alert('Please enter a valid phone number for the selected country.');
-            return false; // Stop the form submission
+            return false; 
         } else{
             alert('welcome this is a valid Number !!!!!!')
         }
 
-        // Here, you could proceed with form submission, for example using an AJAX call
-        // or by uncommenting the following line to allow the form to submit normally.
-        // form.submit();
+
     });
 });
